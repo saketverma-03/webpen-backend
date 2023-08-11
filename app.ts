664f4331452b4a64c094587809f7e6caf93ddcff
saketverma-03 <saketverma831@gmail.com>
2023-08-11 21:25:49 +0530

    update : cookie support

--- app.ts
+++ app.ts
@@ -1,18 +1,29 @@
 import bodyParser from "body-parser";
 
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express, { Request, Response } from "express";
 import prisma from "./prisma";
-const app = express();
-
-app.use(bodyParser.json());
+import projectRouter from "./routes/project";
 import userRoutes from "./routes/user";
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(
+  cors({
+    // origin: "http://localhost:5173",
+    origin: true,
+    credentials: true,
+    // " Access-Control-Allow-Origin": true
+  })
+);
 
 app.use("/api/user", userRoutes);
-
+app.use("/api/projects", projectRouter);
 // get no. of uesrs
 app.get("/", async (req: Request, res: Response) => {
   const users = await prisma.user.count();
   res.json({ message: `${req.body.name}Num,ber of users are ${users}` });
 });
 
-app.listen(3001);
+app.listen(3001, () => console.log("serverStarted at port 3001"));
